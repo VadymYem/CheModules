40ada626994bd51056daa9ce7a16eb149e6b429e
VadymYem <81570039+VadymYem@users.noreply.github.com>
2022-09-14 14:59:48 +0300

    Update AuthorVtt.py

--- AuthorVtt.py
+++ AuthorVtt.py
@@ -14,7 +14,7 @@ from .. import loader
 
 @loader.tds
 class AuthorVttMod(loader.Module):
-    """Распознавание речи через Google Recognition API(pl,en,ru,de)"""
+    """Распознавание речи через Google Recognition API(ua,pl,en,ru,de)"""
 
     strings = {"name": "Author's Voice-to-text(vtt)", "pref": "<b>[Authot's VTT]</b> "}
 
@@ -38,7 +38,7 @@ class AuthorVttMod(loader.Module):
                 audio_content = recog.record(audio_file)
             await m.edit(
                 self.strings["pref"]
-                + recog.recognize_google(audio_content, language="de-DE" and "ru-RU" and "en-EN" and "pl-PL")
+                + recog.recognize_google(audio_content, language="de-DE" and "ru-RU" and "en-EN" and "uk-UK" and "pl-PL")
             )
         else:
             await m.edit(self.strings["pref"] + "reply to audio/voice...")
